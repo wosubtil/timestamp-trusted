333b62cc5a6e468713aca9f575e05824c8a719e3
Welinton <welinton@realizati.com.br>
2019-08-15 20:16:32 -0300

    Finalizando a Response do Carimbo

--- src/trusted-timestamps.ts
+++ src/trusted-timestamps.ts
@@ -3,10 +3,11 @@ import { promisify } from 'util'
 import { stat } from 'fs'
 import tmp from 'tmp'
 
+const statAsync = promisify(stat)
+
 export const createRequestFile = async (hash: string): string => {
   try {
     const createTmpFileAsync = promisify(tmp.file)
-    const statAsync = promisify(stat)
     if (hash.length < 40) {
       throw new Error('Invalid hash')
     }
@@ -23,5 +24,20 @@ export const createRequestFile = async (hash: string): string => {
       throw new Error(error.message)
     }
     throw new Error('There was an error creating File Request')
+  }
+}
+
+export const signRequestFile = async (pathRequestFile: string, urlTSA: string): Promise<string> => {
+  try {
+    await statAsync(pathRequestFile)
+    const pathResponseFile = `${pathRequestFile}.tsr`
+    const command = `curl -H "Content-Type: application/timestamp-query" --data-binary "@${pathRequestFile}" ${urlTSA} > ${pathResponseFile}`
+    await execAsync(command)
+    return pathRequestFile
+  } catch (error) {
+    if (error.message) {
+      throw new Error(error.message)
+    }
+    throw new Error('There was a problem signing the file')
   }
 }
--- tests/trusted-timestamps.test.ts
+++ tests/trusted-timestamps.test.ts
@@ -1,6 +1,8 @@
-import { createRequestFile } from '../src/trusted-timestamps'
+import { createRequestFile, signRequestFile } from '../src/trusted-timestamps'
 import crypto from 'crypto'
 import { stat } from 'fs'
+jest.setTimeout(15000)
+const urlTimeStampAuthority = 'https://freetsa.org/tsr'
 
 describe('createRequestFile', () => {
   test('Smoke tests', () => {
@@ -12,12 +14,35 @@ describe('createRequestFile', () => {
     await expect(createRequestFile(hashFake)).rejects.toThrow(Error)
   })
 
-  test('Should be return a path request files', async () => {
+  test('Should be return a path request file', async () => {
     const rawContent = 'Hello'
     const hashContent = crypto.createHash('sha256').update(rawContent).digest('hex')
     const pathFileRequest = await createRequestFile(hashContent)
     expect(typeof pathFileRequest).toEqual('string')
     stat(pathFileRequest, (error, stats) => {
+      if (error) throw error
+      expect(stats.size).toBeGreaterThan(0)
+    })
+  })
+})
+
+describe('signRequestFile', () => {
+  test('Basic tests', async () => {
+    expect(typeof signRequestFile).toBe('function')
+  })
+
+  test('Should be return error - File Request Invalid', async () => {
+    const pathFileRequestInvalid = '/tmp/123123123'
+    expect(signRequestFile(pathFileRequestInvalid, urlTimeStampAuthority)).rejects.toThrow(Error)
+  })
+
+  test('Should be return a new file signing', async () => {
+    const rawContent = 'Hello'
+    const hashContent = crypto.createHash('sha256').update(rawContent).digest('hex')
+    const pathFileRequest = await createRequestFile(hashContent)
+    const pathResonseFile = await signRequestFile(pathFileRequest, urlTimeStampAuthority)
+    expect(typeof pathResonseFile).toEqual('string')
+    stat(pathResonseFile, (error, stats) => {
       if (error) throw error
       expect(stats.size).toBeGreaterThan(0)
     })
